ea9a95766af5070900d73d2cc9b16c829c0458a5
Ryan Estes <restes93@gmail.com>
2017-11-03 05:40:41 -0500

    completed db connections and examples for schema integration statements. TODO: integration.

--- MongoDB/WordnikAPI.js
+++ MongoDB/WordnikAPI.js
@@ -1,24 +1,9 @@
-var APIKEY = '759d17ade61006ca270010302220f9ff4718cc01d21f069be';
-var Wordnik = require('wordnik-bb').init(APIKEY);
+var Wordnik = require('wordnik');
 
-var word = 'test'
+var wn = new Wordnik({
+    api_key: '759d17ade61006ca270010302220f9ff4718cc01d21f069be'
+});
 
-var word = new Wordnik.Word({word: word, params:{includeSuggestions:true}});
-word.getEverything()
- .then( function() {
-    console.log("A WHOLE lot of data in a Word model: ", word);
-  });
-
-  var randomWordPromise = Wordnik.getRandomWordModel({
-    includePartOfSpeech: "verb-transitive",
-    minCorpusCount: 10000
-  }
-);
-randomWordPromise.done(function(word) {
-  console.log("The model for our random word: ", word);
-  // We could also get more info about the random word:
-  // word.getEverything()
-  //   .then( function() {
-  //      console.log("And now we've populated the model with all the available data: ", word);
-  //   }
+wn.definitions('pernicious', function(e, defs) {
+  console.log(e, defs);
 });
--- MongoDB/mongoose.js
+++ MongoDB/mongoose.js
@@ -1,46 +1,100 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+// This Javascript file connects to the Invisible Ink MongoDB using Mongoose.
+//
+// hostname: Invisibleink
+// password: MeanMachine2017
+// MachineName: InvisibleInk
+// db name: stories
+// collection: story
+//
+// Schema defined below
 
-var.db = mongoose.connection;
+// Global constants for mongoose.
+const mongoose = require('mongoose');
+const Schema = require('mongoose').Schema;
+
+//third-party mongoose library for easier auto incremental '_id'.
+const autoIncrement = require('mongoose-auto-increment');
+
+// Connection URI for MongoDB connection.
+mongoose.connect('mongodb://invisibleink:MeanMachine2017@InvisibleInk/stories');
+
+// Simple check to see if connection was successful.
+var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  // we're connected!
+  console.log('Connected!')
 });
 
+// Third party instantiation for auto _id increment. Only needs declared once.
+autoIncrement.initialize(db);
 
-//Schema declaration for invisible ink.
-// Story ID - automatically instantiated uniquely by MongoDB. -> ObjectID() <-
-// Story
-//   title
-//   author
-//   body
-//   date
-// Likes
-// Comments
+// Schema declaration for stories db.
+//
+//   _id: Number
+//          - unique incremental id that will be automatically given by mongoose-auto-increment.
+//            *doesn't need declared in schema.*
+//   story_title: String
+//   story_author: String
+//   story_body: String (up to 16MB)
+//   data_created: Date
+//   tick_rate: Number
+//   influence_rate: Number
+//   next_event: Number (references the _id)
+//   cycles: Number
+//   likes: Number
+//   views: Number
 
-var InvInkSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    body: String, //LFS
-    comments: [{ author: String, body: String, date: Date }],
-    date: { type: Date, default: Date.now }
+
+var storiesSchema = mongoose.Schema({
+  // _id: Number, This is automatically instantatied and incremented by third-party tool.
+  story_title: String,
+  story_author: String,
+  story_body: String,
+  date_created: { type: Date, default: Date.now }, // timestamp basically
+  tick_rate: Number,
+  influence_rate: Number,
+  next_event: Number,
+  cycles: Number,
+  likes: Number,
+  views: Number
 });
 
+storiesSchema.plugin(autoIncrement.plugin, 'Story'); //LoC that increments _id field.
 
-//stores the body in GridFS
-fileSchema.plugin(mongooseFS, {keys: ['body'], mongoose: mongoose});
-var File = mongoose.model('File', fileSchema);
+// Creation of model which is a class in which mongoose creates MongoDB Documents.
+var Story = mongoose.model('Story', storiesSchema);
+module.exports = Story;
+
+// At this point we can start adding and pulling to and from the DB.
 
 
-//Loading a document.
-File.findById(id, function (err, file) {
-  if(err) {
-    return done(err);
-  }
-  file.retrieveBlobs(function (err) {
-    if(err) {
-      return done(err);
-    }
-    // Now everything is ready !
-  });
-});
+
+// Example Query For Entire Schema:
+//
+// var newStory = new Story({
+//   story_title: <Insert Title Here>,
+//   story_author: <Insert Author Here>,
+//   story_body: <Insert Body Here>,
+//   tick_rate: <Insert tick rate Here>,
+//   influence_rate: <Insert influence rate Here>,
+//   next_event: <Insert next event _id number Here>,
+//   cycles: <Insert cycles Here>,
+//   likes: <Insert likes Here>,
+//   views: 0       // will be 0 on creation
+// };
+//
+// Story..save(function(err) {
+//   console.log('User saved successfully!');
+//   if (err) throw err;
+// });
+
+
+// Example Find and Update query that will be used every single time a user views,
+// likes, and rates degrades, body changes form degredation, etc.:
+//
+// Lets say for this example someone viewed the story. We need to create a small
+// query with the updated views amount and then find the story it is related too
+// and then update it.
+//
+// var viewsUpdate = { <_id of document> }; //Story.findbyId(req.params._id)
+// Story.findOneAndUpdate(viewsUpdate, { views: <insert new views amount here> }, { 'new': false }, function (err, res) {} );
