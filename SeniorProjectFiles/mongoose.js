dec7ab813031de4a561f084f1a0ed6b6c2cb45f2
Ryan Estes <restes93@gmail.com>
2017-11-16 13:32:54 -0600

    Added all queries to include id and added story type to schema.

--- SeniorProjectFiles/mongoose.js
+++ SeniorProjectFiles/mongoose.js
@@ -31,6 +31,7 @@ var stories = mongoose.Schema({
   story_title: String,
   story_author: String,
   story_body: String,
+  story_type: String,
   date_created: { type: Date, default: Date.now }, // timestamp
   tick_rate: Number,
   influence_rate: Number,
@@ -47,14 +48,15 @@ var Story = mongoose.model('Story', stories);
 module.exports = Story;
 
 // INSERT query for a new story. (CONSTRUCTOR)
-function createStory(title, author, tickRate, infRate){
+function createStory(){
   var newStory = new Story({
-     story_title: title,
-     story_author: author,
-     story_body: getBody(),
-     tick_rate: tickRate,
-     influence_rate: infRate,
-     next_event: getNextEvent(),
+     story_title: "",
+     story_author: "",
+     story_body: "",
+	 story_type: "",
+     tick_rate: 0,
+     influence_rate: 0,
+     next_event: 0,
      cycles: 0,
      likes: 0,
      views: 0
@@ -68,73 +70,95 @@ function createStory(title, author, tickRate, infRate){
 
 
 // GETTERS
-function getTitle(){
-  Story.findOne({ 'story_title': this.stories[0] }, function (err, title) {
+function getID(id){
+	Story.findById(id, function (err, id) {
     if (err) return handleError(err);
-    console.log('%s found!', title)
+    return id;
   });
 };
 
-function getAuthor(){
-  Story.findOne({ 'story_author': this.stories[1] }, function (err, author) {
+
+//function getTitle(){
+  //Story.findOne({ 'story_title': this.stories[0] }, function (err, title) {
+    //if (err) return handleError(err);
+    //console.log('%s found!', title)
+  //});
+//};
+
+function getTitle(id){
+  Story.findById(id, function (err, title) {
     if (err) return handleError(err);
-    console.log('%s found!', author)
+    return Story.story_title
   });
 };
 
-function getBody(){
-  Story.findOne({ 'story_body': this.stories[2] }, function (err, body) {
+function getAuthor(id){
+  Story.findById(id, function (err, author) {
     if (err) return handleError(err);
-    console.log('%s found!', body)
+    return Story.story_author;
   });
 };
 
-function getDateCreated(){
-  Story.findOne({ 'date_created': this.stories[3] }, function (err, date) {
+function getBody(id){
+  Story.findById(id, function (err, body) {
     if (err) return handleError(err);
-    console.log('%s found!', date)
+    return Story.story_body;
   });
 };
 
-function getTickRate(){
-  Story.findOne({ 'tick_rate': this.stories[4] }, function (err, tick) {
+function getType(id){
+  Story.findById(id, function (err, type) {
     if (err) return handleError(err);
-    console.log('%s found!', tick)
+    return Story.story_type;
   });
 };
 
-function getInfluenceRate(){
-  Story.findOne({ 'influence_rate': this.stories[5] }, function (err, inf) {
+function getDateCreated(id){
+  Story.findById(id, function (err, date) {
     if (err) return handleError(err);
-    console.log('%s found!', inf)
+    return Story.date_created;
   });
 };
 
-function getNextEvent(){
-  Story.findOne({ 'next_event': this.stories[6] }, function (err, nexte) {
+function getTickRate(id){
+  Story.findById(id, function (err, tick) {
     if (err) return handleError(err);
-    console.log('%s found!', nexte)
+    return Story.tick_rate;
   });
 };
 
-function getCycles(){
-  Story.findOne({ 'cycles': this.stories[7] }, function (err, cycle) {
+function getInfluenceRate(id){
+  Story.findById(id, function (err, inf) {
     if (err) return handleError(err);
-    console.log('%s found!', cycle)
+    return Story.influence_rate;
   });
 };
 
-function getLikes(){
-  Story.findOne({ 'likes': this.stories[8] }, function (err, like) {
+function getNextEvent(id){
+  Story.findById(id, function (err, nexte) {
     if (err) return handleError(err);
-    console.log('%s found!', like)
+    return Story.next_event;
   });
 };
 
-function getViews(){
-  Story.findOne({ 'views': this.stories[9] }, function (err, view) {
+function getCycles(id){
+  Story.findById(id, function (err, cycle) {
     if (err) return handleError(err);
-    console.log('%s found!', view)
+    return Story.cycles;
+  });
+};
+
+function getLikes(id){
+  Story.findById(id, function (err, like) {
+    if (err) return handleError(err);
+    return Story.likes;
+  });
+};
+
+function getViews(id){
+  Story.findById(id, function (err, view) {
+    if (err) return handleError(err);
+    return Story.views;
   });
 };
 
@@ -144,60 +168,53 @@ function getViews(){
 
 // Unique getter that is used as a find function for the current story.
 // This uses the author and title to find the current document.
-function query(){
-  var query = { 'story_author': author, 'story_title': title };
+function query(id){
+  var query = { '_id': id };
   // then update according to the current document setter.
 }
 
 
 // Find and Update Queries (SETTERS)
-function setViews(title){
-  Story.update(query(), { $set: { story_title: title}}, callback);
+function setTitle(id, title){
+  Story.update(query(id), { $set: { story_title: title}}, callback);
 };
 
-function setViews(author){
-  Story.update(query(), { $set: { story_author: author}}, callback);
+function setAuthor(id, author){
+  Story.update(query(id), { $set: { story_author: author}}, callback);
 };
 
-function setViews(body){
-  Story.update(query(), { $set: { story_body: body}}, callback);
+function setBody(id, body){
+  Story.update(query(id), { $set: { story_body: body}}, callback);
 };
 
-function setViews(dateCreated){
-  Story.update(query(), { $set: { date_created: dateCreated}}, callback);
+function setType(id, type){
+  Story.update(query(id), { $set: { story_type: type}}, callback);
 };
 
-function setViews(ticks){
-  Story.update(query(), { $set: { tick_rate: ticks}}, callback);
+function setDateCreated(id, dateCreated){
+  Story.update(query(id), { $set: { date_created: dateCreated}}, callback);
 };
 
-function setViews(inf_rate){
-  Story.update(query(), { $set: { influence_rate: inf_rate}}, callback);
+function setTicks(id, ticks){
+  Story.update(query(id), { $set: { tick_rate: ticks}}, callback);
 };
 
-function setViews(next_e){
-  Story.update(query(), { $set: { next_event: next_e}}, callback);
+function setInfRate(id, inf_rate){
+  Story.update(query(id), { $set: { influence_rate: inf_rate}}, callback);
 };
 
-function setViews(cyc){
-  Story.update(query(), { $set: { cycles: cyc}}, callback);
+function setNextEvent(id, next_e){
+  Story.update(query(id), { $set: { next_event: next_e}}, callback);
 };
 
-function setViews(like){
-  Story.update(query(), { $set: { likes: like}}, callback);
+function setCycles(id, cyc){
+  Story.update(query(id), { $set: { cycles: cyc}}, callback);
 };
 
-function setViews(v){
-  Story.update(query(), { $set: { views: v}}, callback);
+function setLikes(id, like){
+  Story.update(query(id), { $set: { likes: like}}, callback);
 };
 
-
-// Example Find and Update query that will be used every single time a user views,
-// likes, and rates degrades, body changes form degredation, etc.:
-//
-// Lets say for this example someone viewed the story. We need to create a small
-// query with the updated views amount and then find the story it is related too
-// and then update it.
-//
-// var viewsUpdate = { <_id of document> }; //Story.findbyId(req.params._id)
-// Story.findOneAndUpdate(viewsUpdate, { views: <insert new views amount here> }, { 'new': false }, function (err, res) {} );
+function setViews(id, v){
+  Story.update(query(id), { $set: { views: v}}, callback);
+};
